2cb7a79cca2cbbf7f73dc4ce4035aa756aeea653
abagi <abagi@ucsc.edu>
2021-01-06 12:57:45 -0800

    addition of rn6 to quickdb, independence of quickdb_trax.bash from docker, cleanup of pyc files, cleanup to both quickstart commands syntax and help commands

--- quickdb_trax.bash
+++ quickdb_trax.bash
@@ -1,42 +1,60 @@
 #!/usr/bin/env bash
 
+# Supported genomes
+GENOMES=("hg19" "hg38" "rn6" "mm10" "sacCer3")
+
+# Help function
+function print_usage() {
+  echo "USAGE: $0 databasename location(optional)" >&2
+  echo "  databasename: ${GENOMES[@]}" >&2
+  echo "  location: Directory to store the database (default = /rnadb)" >&2
+}
+
 # Function to build the database
 function db_builder() {
   if test "${1}" = "hg19"
-  then
-     GTF_URL="ftp://ftp.ensembl.org/pub/grch37/current/gtf/homo_sapiens/Homo_sapiens.GRCh37.87.gtf.gz"
-     gtRNAdb_URL="http://gtrnadb.ucsc.edu/genomes/eukaryota/Hsapi19/hg19-tRNAs.tar.gz"
-     gtRNAdb_OUT="hg19-tRNAs-detailed.out"
-     gtRNAdb_NAME="hg19-tRNAs_name_map.txt"
-     GENOME_URL="http://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz"
-     FASTA=true
+    then
+       GTF_URL="ftp://ftp.ensembl.org/pub/grch37/current/gtf/homo_sapiens/Homo_sapiens.GRCh37.87.gtf.gz"
+       gtRNAdb_URL="http://gtrnadb.ucsc.edu/genomes/eukaryota/Hsapi19/hg19-tRNAs.tar.gz"
+       gtRNAdb_OUT="hg19-tRNAs-detailed.out"
+       gtRNAdb_NAME="hg19-tRNAs_name_map.txt"
+       GENOME_URL="http://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz"
+       FASTA=true
   elif test "${1}" = "hg38"
-  then
-     GTF_URL="ftp://ftp.ensembl.org/pub/release-102/gtf/homo_sapiens/Homo_sapiens.GRCh38.102.gtf.gz"
-     gtRNAdb_URL="http://gtrnadb.ucsc.edu/GtRNAdb2/genomes/eukaryota/Hsapi38/hg38-tRNAs.tar.gz"
-     gtRNAdb_OUT="hg38-tRNAs-detailed.out"
-     gtRNAdb_NAME="hg38-tRNAs_name_map.txt"
-     GENOME_URL="http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz"
-     FASTA=true
+    then
+       GTF_URL="ftp://ftp.ensembl.org/pub/release-102/gtf/homo_sapiens/Homo_sapiens.GRCh38.102.gtf.gz"
+       gtRNAdb_URL="http://gtrnadb.ucsc.edu/GtRNAdb2/genomes/eukaryota/Hsapi38/hg38-tRNAs.tar.gz"
+       gtRNAdb_OUT="hg38-tRNAs-detailed.out"
+       gtRNAdb_NAME="hg38-tRNAs_name_map.txt"
+       GENOME_URL="http://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz"
+       FASTA=true
  elif test "${1}" = "mm10"
- then
-    GTF_URL="ftp://ftp.ensembl.org/pub/release-102/gtf/mus_musculus/Mus_musculus.GRCm38.102.gtf.gz"
-    gtRNAdb_URL="http://gtrnadb.ucsc.edu/genomes/eukaryota/Mmusc10/mm10-tRNAs.tar.gz"
-    gtRNAdb_OUT="mm10-tRNAs-confidence-set.out"
-    gtRNAdb_NAME="mm10-tRNAs_name_map.txt"
-    GENOME_URL="https://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/mm10.2bit"
-    FASTA=false
+   then
+      GTF_URL="ftp://ftp.ensembl.org/pub/release-102/gtf/mus_musculus/Mus_musculus.GRCm38.102.gtf.gz"
+      gtRNAdb_URL="http://gtrnadb.ucsc.edu/genomes/eukaryota/Mmusc10/mm10-tRNAs.tar.gz"
+      gtRNAdb_OUT="mm10-tRNAs-confidence-set.out"
+      gtRNAdb_NAME="mm10-tRNAs_name_map.txt"
+      GENOME_URL="https://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/mm10.2bit"
+      FASTA=false
+ elif test "${1}" = "rn6"
+   then
+      GTF_URL="ftp://ftp.ensembl.org/pub/release-102/gtf/rattus_norvegicus/Rattus_norvegicus.Rnor_6.0.102.gtf.gz"
+      gtRNAdb_URL="http://gtrnadb.ucsc.edu/genomes/eukaryota/Rnorv6/rn6-tRNAs.tar.gz"
+      gtRNAdb_OUT="rn6-tRNAs-detailed.out"
+      gtRNAdb_NAME="rn6-tRNAs_name_map.txt"
+      GENOME_URL="https://hgdownload.soe.ucsc.edu/goldenPath/rn6/bigZips/rn6.fa.gz"
+      FASTA=true
  elif test "${1}" = "sacCer3"
- then
-    GTF_URL="ftp://ftp.ensembl.org/pub/release-97/gtf/saccharomyces_cerevisiae/Saccharomyces_cerevisiae.R64-1-1.97.gtf.gz"
-    gtRNAdb_URL="http://gtrnadb.ucsc.edu/genomes/eukaryota/Scere3/sacCer3-tRNAs.tar.gz"
-    gtRNAdb_OUT="sacCer3-tRNAs.out-noChrM"
-    gtRNAdb_NAME="sacCer3-tRNAs_name_map.txt"
-    GENOME_URL="https://hgdownload.soe.ucsc.edu/goldenPath/sacCer3/bigZips/sacCer3.2bit"
-    FASTA=false
+   then
+      GTF_URL="ftp://ftp.ensembl.org/pub/release-97/gtf/saccharomyces_cerevisiae/Saccharomyces_cerevisiae.R64-1-1.97.gtf.gz"
+      gtRNAdb_URL="http://gtrnadb.ucsc.edu/genomes/eukaryota/Scere3/sacCer3-tRNAs.tar.gz"
+      gtRNAdb_OUT="sacCer3-tRNAs.out-noChrM"
+      gtRNAdb_NAME="sacCer3-tRNAs_name_map.txt"
+      GENOME_URL="https://hgdownload.soe.ucsc.edu/goldenPath/sacCer3/bigZips/sacCer3.2bit"
+      FASTA=false
   else
-    echo "Could not generate RNA database"
-    return
+      echo "Could not generate RNA database, non-supported DB or parameter missing"
+      return
   fi
 
   # GTF File from Ensembl
@@ -47,39 +65,50 @@ function db_builder() {
     awk '{print "chr" $0;}' | \
     sed 's/chrMT/chrM/g' | \
     grep -e Mt_rRNA -e Mt_tRNA -e miRNA -e misc_RNA -e rRNA -e snRNA -e snoRNA -e ribozyme -e sRNA -e scaRNA \
-    > /rnadb/genes.gtf
+    > ${DB_LOCATION}/genes.gtf
   echo "Generating GTF Done"
 
   # gtRNAdb Files
   echo "Generating gtRNAdb"
-  wget -q -O /rnadb/tse.tar.gz ${gtRNAdb_URL}
-  tar zxf /rnadb/tse.tar.gz -C /rnadb
-  rm /rnadb/tse.tar.gz
+  wget -q -O ${DB_LOCATION}/tse.tar.gz ${gtRNAdb_URL}
+  tar zxf ${DB_LOCATION}/tse.tar.gz -C ${DB_LOCATION}
+  rm ${DB_LOCATION}/tse.tar.gz
   echo "Generating gtRNAdb Done"
 
   # Genome Fasta File from UCSC
   echo "Generating Fasta"
   if test ${FASTA} = true
-  then
-    wget -q -O - ${GENOME_URL} | gzip -cd > /rnadb/genome.fa
+    then
+      wget -q -O - ${GENOME_URL} | gzip -cd > ${DB_LOCATION}/genome.fa
   else
-    wget -q -O /rnadb/genome.2bit ${GENOME_URL}
-    wget -q -O /rnadb/twoBitToFa http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/twoBitToFa
-    chmod +x /rnadb/twoBitToFa
-    /rnadb/twoBitToFa /rnadb/genome.2bit /rnadb/genome.fa
+    wget -q -O ${DB_LOCATION}/genome.2bit ${GENOME_URL}
+    wget -q -O ${DB_LOCATION}/twoBitToFa http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/twoBitToFa
+    chmod +x ${DB_LOCATION}/twoBitToFa
+    ${DB_LOCATION}/twoBitToFa ${DB_LOCATION}/genome.2bit ${DB_LOCATION}/genome.fa
   fi
   echo "Generating Fasta Done"
 
   # TRAX maketrnadb
   echo "Starting TRAX makernadb"
   maketrnadb.py \
-    --databasename=/rnadb/db \
-    --genomefile=/rnadb/genome.fa \
-    --trnascanfile=/rnadb/${gtRNAdb_OUT} \
-    --namemapfile=/rnadb/${gtRNAdb_NAME}
-
-  # Change permissions
-  #chmod -R 777 /rnadb/
+    --databasename=${DB_LOCATION}/db \
+    --genomefile=${DB_LOCATION}/genome.fa \
+    --trnascanfile=${DB_LOCATION}/${gtRNAdb_OUT} \
+    --namemapfile=${DB_LOCATION}/${gtRNAdb_NAME}
 }
 
-db_builder ${1}
+# Init test
+if [ -z "$2" ]
+  then
+    DB_LOCATION="/rnadb"
+else
+  DB_LOCATION=${2}
+  mkdir ${DB_LOCATION}
+fi
+
+if [ -z "$1" ]
+  then
+    print_usage
+else
+db_builder ${1} ${DB_LOCATION}
+fi
--- quickstart_trax.bash
+++ quickstart_trax.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Supported genomes
-GENOMES=("hg19" "hg38" "mm10" "sacCer3")
+GENOMES=("hg19" "hg38" "rn6" "mm10" "sacCer3")
 
 # Help function
 function print_usage() {
@@ -9,31 +9,10 @@ function print_usage() {
   echo "  tool: make, build, run, manual" >&2
   echo "    make: Build the docker container" >&2
   echo "    build: Build RNA database into a docker volume" >&2
-  echo "    run: Run TRAX with a prebuilt docker volume, must also supply the following arguments" >&2
-  echo "      experiment_name: The name for your samples that will be made as a folder in the data parameter" >&2
-  echo "      sample_file: The supplied sample file located under the data parameter" >&2
   echo "    manual: Run container with prebuilt docker volume" >&2
-  echo "  databasename: hg19, hg38, mm10, sacCer3" >&2
+  echo "  databasename: ${GENOMES[@]}" >&2
   echo "  data: Directory to mount with the data (optional)" >&2
 }
-
-# Function to start trax automatically and run
-#function docker_trax() {
-#  if [ -z "$4" ]
-#  then
-#    print_usage
-#  else
-#    docker run --rm -it --name trax-${USER} \
-#      --user=`id -u`:`id -g` \
-#      -v rnadb-${1}:/rnadb \
-#      -v ${2}:${2} \
-#      trax ./processsamples.py \
-#      --experimentname=/home/jerry/data/output \
-#      --databasename=/rnadb/db \
-#      --samplefile=/home/jerry/data/${4} \
-#      --ensemblgtf=/rnadb/genes.gtf
-#  fi
-#}
 
 # Function to build the Docker container
 function docker_make() {
@@ -42,11 +21,17 @@ function docker_make() {
 
 # Function to start the container and build a RNA database
 function docker_build_db() {
-  docker volume create rnadb-${1}
-  docker run --rm -it --name trax-build-rnadb-${1} \
-    -v rnadb-${1}:/rnadb \
-    trax \
-    quickdb_trax.bash ${1}
+  if [ -z "$1" ]
+    then
+      echo "supply a databasename: ${GENOMES[@]}"
+  else
+    docker volume create rnadb-${1}
+    docker run --rm -it --name trax-build-rnadb-${1} \
+      -v `pwd`:/opt/trax \
+      -v rnadb-${1}:/rnadb \
+      trax \
+      quickdb_trax.bash ${1}
+  fi
 }
 
 # Function to start a manual Docker TRAX container
@@ -54,24 +39,17 @@ function docker_manual() {
   docker run --rm -it --name trax-${USER}-2 \
   --user=`id -u`:`id -g` \
   -v rnadb-${1}:/rnadb \
-  #for i in ${@:2}
-  #do
-  #  -v  $i:$i
-  #done
   trax
 }
 
 # Init function
-if test ${1} = "make"
+if [[ ${1} = "make" ]]
 then
  docker_make
-elif test ${1} = "build"
+elif [[ ${1} = "build" ]]
 then
   [[ ${GENOMES[*]} =~ ${2} ]] && docker_build_db ${@:2} || print_usage
-#elif test ${1} = "run"
-#then
-#  [[ ${GENOMES[*]} =~ ${2} ]] && docker_trax ${@:2} || print_usage
-elif test ${1} = "manual"
+elif [[ ${1} = "manual" ]]
 then
   [[ ${GENOMES[*]} =~ ${2} ]] && docker_manual ${@:2} || print_usage
 else
